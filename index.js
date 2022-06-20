775a276572702b72b7c854cecd4c979cc50a3ef3
Echo974618105 <974618105@qq.com>
2022-06-20 19:14:58 +0800

    第一次上传了文件

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Echo974618105 974618105@qq.com
+ * @Date: 2022-06-20 15:10:46
+ * @LastEditors: 张明辉 11207616+zhangminghui0520@user.noreply.gitee.com
+ * @LastEditTime: 2022-06-20 18:57:44
+ * @FilePath: \前端d:\Users\zmh\就业班\案例练习\git\day01\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+11
+11
+11
